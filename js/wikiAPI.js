91eba1a88b40d43f3b0c0a6588b53d2b809905ee
Amondt <mondt.alexande>
2019-02-21 15:52:13 +0100

    wiki page search done

--- js/wikiAPI.js
+++ js/wikiAPI.js
@@ -3,6 +3,8 @@ window.onload = function () {
     var suggest = ''
     var dataList = document.querySelector('#suggest')
 
+    var logo = document.querySelector('#logo')
+
     // Event text helper
     var text = document.querySelector('#text')
     var aideText = document.querySelector('#aideText')
@@ -54,12 +56,17 @@ window.onload = function () {
         e.preventDefault()
         
         // Transition logo / form up (hide logo)
-        document.querySelector('#logo').style.top = '-150px'
-        // wikiSearch
-        // logo
+        logo.style.width = '0%'
+        logo.style.height = '0%'
+        logo.style.margin = '0'
+        logo.style.padding = '0'
+
+        // Hide form display
+        document.querySelector('#wikiSearch').style.display = 'none'
+        document.querySelector('#showSearch').style.display = 'flex'
 
         // Erase suggestions on enter / click
-        for (i=0; i<suggest[1].length; i++) {
+        for (i=1; i<11; i++) {
             dataList.childNodes[i].value = ''
         }
 
@@ -79,9 +86,27 @@ window.onload = function () {
         for (i=0; i<10; i++) {
             if (text.value === suggest[1][i]) {
                 var src = suggest[3][i]
+            } else {
+                var src = suggest[3][i]
             }
         }
         iframe.src = src
         container.appendChild(iframe)
+
+        // Event hide search bar on mouseover wiki iframe
+        document.querySelector('#containerWiki').addEventListener('mouseover', function(e) {
+            document.querySelector('#wikiSearch').style.display = 'none'
+            document.querySelector('#showSearch').style.display = 'flex'
+        })
+    })
+
+    // Event show search bar on mouseover new search top bar
+    document.querySelector('#showSearch').addEventListener('mouseover', function(e) {
+        e.preventDefault()
+
+        text.value = ''
+
+        document.querySelector('#wikiSearch').style.display = 'flex'
+        document.querySelector('#showSearch').style.display = 'none'
     })
 }
