2b3e77c93ce7712c8695aa4dac778d828d2178de
Amondt <mondt.alexande>
2019-02-21 12:36:53 +0100

    wip wiki page transitions

--- js/wikiAPI.js
+++ js/wikiAPI.js
@@ -1,5 +1,8 @@
 window.onload = function () {
 
+    var suggest = ''
+    var dataList = document.querySelector('#suggest')
+
     // Event text helper
     var text = document.querySelector('#text')
     var aideText = document.querySelector('#aideText')
@@ -13,62 +16,72 @@ window.onload = function () {
     })
 
     // Event keypress enter
-    text.addEventListener('keypress', function(e) {
+    text.addEventListener('keypress', keyPress)
+    
+    function keyPress(e) {
         if (e.keyCode === 13) {
             e.preventDefault()
             document.querySelector('#wikiBtn').click()
             text.value = ''
+        } else {
+            // e.preventDefault()
+            text.removeEventListener('keypress', keyPress)
+            setTimeout(function() {
+                const req = new XMLHttpRequest()
+                req.open('GET', 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+e.target.value, false)
+                req.send(null)
+
+                if (req.status === 200) {
+                    console.log('rep recue : %s', req.status)
+
+                    suggest = JSON.parse(req.response)
+                    console.log(suggest[1].length, suggest[1])
+                    console.log(dataList.childNodes)
+                    for (i=0; i<10; i++) {
+                        dataList.childNodes[i+1].value = suggest[1][i]
+                    }
+                    text.addEventListener('keypress', keyPress)
+
+                } else {
+                    console.log('status de la rep: %s (%d)', req.status, req.statusText)
+                }
+            }, 500)
         }
-    })
+    }
 
     // Event search click
     document.querySelector('#wikiBtn').addEventListener('click', function(e) {
         e.preventDefault()
+        
+        // Transition logo / form up (hide logo)
+        document.querySelector('#logo').style.top = '-150px'
+        // wikiSearch
+        // logo
 
+        // Erase suggestions on enter / click
+        for (i=0; i<suggest[1].length; i++) {
+            dataList.childNodes[i].value = ''
+        }
+
+        // Remove old container / iframe
         if (document.querySelector('#containerWiki')) {
-            // document.querySelector('#containerWiki').parentNode.removeChild(document.querySelector('#containerWiki'))
+
             document.querySelector('#containerWiki').remove()
         }
 
         const container = document.createElement('div')
-        container.setAttribute('class', 'container')
+        container.setAttribute('class', 'contain')
         container.id = 'containerWiki'
         document.body.appendChild(container)
 
-        const req = new XMLHttpRequest()
-        req.open('GET', 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+text.value, false)
-        req.send(null)
-
-        if (req.status === 200) {
-            console.log('rep recue : %s', req.status)
-            // Start display API request info's
-            let data = JSON.parse(req.response)
-
-            for (i=0; i<data[1].length; i++) {
-                const part = document.createElement('div')
-                part.setAttribute('class', 'promo') // Same css than last API exercice
-
-                const result = document.createElement('h3')
-                result.setAttribute('class', 'result')
-                result.textContent = data[1][i]
-
-                const description = document.createElement('p')
-                description.setAttribute('class', 'description')
-                description.textContent = data[2][i]
-
-                const wiki = document.createElement('a')
-                wiki.setAttribute('class', 'wiki')
-                wiki.textContent = 'wiki'
-                wiki.href = data[3][i]
-
-                container.appendChild(part)
-                part.appendChild(result)
-                part.appendChild(description)
-                part.appendChild(wiki)
+        const iframe = document.createElement('iframe')
+        iframe.id = 'iframe'
+        for (i=0; i<10; i++) {
+            if (text.value === suggest[1][i]) {
+                var src = suggest[3][i]
             }
-            text.value = ''
-        } else {
-            console.log('status de la rep: %s (%d)', req.status, req.statusText)
         }
+        iframe.src = src
+        container.appendChild(iframe)
     })
 }
